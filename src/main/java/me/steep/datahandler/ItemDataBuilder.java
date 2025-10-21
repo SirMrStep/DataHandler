6a1bf8db2c9b72a63482301e6e69169ed302048d
Steep <lemikahriman@gmail.com>
2025-10-21 16:11:47 +0200

    Changelog:
    - Optimized ItemDataBuilder
    - Added addAttributeModifier to ItemDataBuilder

--- src/main/java/me/steep/datahandler/ItemDataBuilder.java
+++ src/main/java/me/steep/datahandler/ItemDataBuilder.java
@@ -1,6 +1,8 @@
 package me.steep.datahandler;
 
 import com.jeff_media.morepersistentdatatypes.DataType;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
@@ -12,10 +14,10 @@ import static me.steep.datahandler.Key.key;
 
 @SuppressWarnings("all")
 public class ItemDataBuilder {
-    
+
     private final ItemStack itemStack;
     private final ItemMeta meta;
-    
+
     public ItemDataBuilder(ItemStack itemStack) {
         this.itemStack = itemStack;
         this.meta = itemStack.getItemMeta();
@@ -28,7 +30,6 @@ public class ItemDataBuilder {
     public ItemDataBuilder setNameAndLore(String name, String... lore) {
         meta.setDisplayName(name);
         meta.setLore(Arrays.asList(lore));
-        itemStack.setItemMeta(meta);
         return this;
     }
 
@@ -39,7 +40,6 @@ public class ItemDataBuilder {
     public ItemDataBuilder setNameAndLore(String name, List<String> lore) {
         meta.setDisplayName(name);
         meta.setLore(lore);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
@@ -48,7 +48,6 @@ public class ItemDataBuilder {
      */
     public ItemDataBuilder setLore(String... lore) {
         meta.setLore(Arrays.asList(lore));
-        itemStack.setItemMeta(meta);
         return this;
     }
 
@@ -57,7 +56,6 @@ public class ItemDataBuilder {
      */
     public ItemDataBuilder setLore(List<String> lore) {
         meta.setLore(lore);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
@@ -66,7 +64,12 @@ public class ItemDataBuilder {
      */
     public ItemDataBuilder setName(String name) {
         meta.setDisplayName(name);
-        itemStack.setItemMeta(meta);
+
+        return this;
+    }
+
+    public ItemDataBuilder addAttributeModifier(Attribute attribute, AttributeModifier modifier) {
+        meta.addAttributeModifier(attribute, modifier);
         return this;
     }
 
@@ -75,12 +78,11 @@ public class ItemDataBuilder {
      */
     public ItemDataBuilder removeData(String key) {
         meta.getPersistentDataContainer().remove(key(key));
-        itemStack.setItemMeta(meta);
         return this;
     }
 
     /**
-     * @param key The key for the Object stored in the specified ItemStack's PersistentDataContainer
+     * @param key  The key for the Object stored in the specified ItemStack's PersistentDataContainer
      * @param type The type of Object you are storing (you can use DataType for this)
      */
     public ItemDataBuilder setData(String key, PersistentDataType type, Object object) {
@@ -90,95 +92,87 @@ public class ItemDataBuilder {
 
     /**
      * @param key The key to the String stored in the ItemStack's PersistentDataContainer
-     * @param s The String to store in the specified ItemStack's PersistentDataContainer
+     * @param s   The String to store in the specified ItemStack's PersistentDataContainer
      */
     public ItemDataBuilder setDataString(String key, String s) {
         meta.getPersistentDataContainer().set(key(key), DataType.STRING, s);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
     /**
      * @param key The key to the Boolean stored in the ItemStack's PersistentDataContainer
-     * @param b The Boolean to store in the specified ItemStack's PersistentDataContainer
+     * @param b   The Boolean to store in the specified ItemStack's PersistentDataContainer
      */
     public ItemDataBuilder setDataBoolean(String key, Boolean b) {
         meta.getPersistentDataContainer().set(key(key), DataType.BOOLEAN, b);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
     /**
      * @param key The key to the Integer stored in the ItemStack's PersistentDataContainer
-     * @param i The Integer to store in the specified ItemStack's PersistentDataContainer
+     * @param i   The Integer to store in the specified ItemStack's PersistentDataContainer
      */
     public ItemDataBuilder setDataInt(String key, Integer i) {
         meta.getPersistentDataContainer().set(key(key), DataType.INTEGER, i);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
     /**
      * @param key The key to the Double stored in the ItemStack's PersistentDataContainer
-     * @param d The Double to store in the specified ItemStack's PersistentDataContainer
+     * @param d   The Double to store in the specified ItemStack's PersistentDataContainer
      */
     public ItemDataBuilder setDataDouble(String key, Double d) {
         meta.getPersistentDataContainer().set(key(key), DataType.DOUBLE, d);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
     /**
      * @param key The key to the Float stored in the ItemStack's PersistentDataContainer
-     * @param f The Float to store in the specified ItemStack's PersistentDataContainer
+     * @param f   The Float to store in the specified ItemStack's PersistentDataContainer
      */
     public ItemDataBuilder setDataFloat(String key, Float f) {
         meta.getPersistentDataContainer().set(key(key), DataType.FLOAT, f);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
     /**
      * @param key The key to the Long stored in the ItemStack's PersistentDataContainer
-     * @param l The Long to store in the specified ItemStack's PersistentDataContainer
+     * @param l   The Long to store in the specified ItemStack's PersistentDataContainer
      */
     public ItemDataBuilder setDataLong(String key, Long l) {
         meta.getPersistentDataContainer().set(key(key), DataType.LONG, l);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
     /**
      * @param key The key to the Short stored in the ItemStack's PersistentDataContainer
-     * @param b The Short to store in the specified ItemStack's PersistentDataContainer
+     * @param b   The Short to store in the specified ItemStack's PersistentDataContainer
      */
     public ItemDataBuilder setDataShort(String key, Short s) {
         meta.getPersistentDataContainer().set(key(key), DataType.SHORT, s);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
     /**
      * @param key The key to the Byte stored in the ItemStack's PersistentDataContainer
-     * @param b The Byte to store in the specified ItemStack's PersistentDataContainer
+     * @param b   The Byte to store in the specified ItemStack's PersistentDataContainer
      */
     public ItemDataBuilder setDataByte(String key, Byte b) {
         meta.getPersistentDataContainer().set(key(key), DataType.BYTE, b);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
     /**
-     * @param key The key to the ItemStack stored in the ItemStack's PersistentDataContainer
+     * @param key     The key to the ItemStack stored in the ItemStack's PersistentDataContainer
      * @param toStore The ItemStack to store in the specified ItemStack's PersistentDataContainer
      */
     public ItemDataBuilder setDataItemStack(String key, ItemStack toStore) {
         meta.getPersistentDataContainer().set(key(key), DataType.ITEM_STACK, toStore);
-        itemStack.setItemMeta(meta);
         return this;
     }
 
     public ItemStack item() {
+        this.itemStack.setItemMeta(meta);
         return this.itemStack;
     }
 
